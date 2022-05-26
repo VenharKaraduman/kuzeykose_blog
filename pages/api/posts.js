a88754069048708a5fc0542f9851678a8bcd6312
Kuzey Köse <kuzeykose@Kuzeys-MacBook-Pro.local>
2022-05-26 10:42:40 +0300

    res.setHeaders added

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -3,5 +3,15 @@ import { getPosts } from "../../src/blog-posts";
 const posts = getPosts();
 
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   res.status(200).json({ posts });
 }
