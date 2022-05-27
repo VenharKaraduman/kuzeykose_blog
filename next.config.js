54ede5f086102eb6081965a14b57798126ee2608
Kuzey Köse <kuzeykose@Kuzeys-MacBook-Pro.local>
2022-05-27 10:31:25 +0300

    test

--- next.config.js
+++ next.config.js
@@ -15,7 +15,6 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://kuzeykose-blog.vercel.app" },
         ],
       },
     ];
