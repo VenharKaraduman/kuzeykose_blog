30c6f452ab28b83873145b73f1f6ff05dd6a32d9
Kuzey Köse <kuzeykose@Kuzeys-MacBook-Pro.local>
2022-05-27 11:08:27 +0300

    Cors added

--- pages/api/post/[postId].js
+++ pages/api/post/[postId].js
@@ -1,9 +1,20 @@
+import Cors from "cors";
+import initMiddleware from "../../lib/init-middleware";
 import { getPosts } from "../../../src/blog-posts";
+ await cors(req, res)
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
 
 const posts = getPosts();
-
+await cors(req, res);
 export default (req, res) => {
   res.json({
-    post: posts.find(post => post.slug === req.query.postId)
+    post: posts.find((post) => post.slug === req.query.postId),
   });
 };
--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,7 +1,18 @@
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
 import { getPosts } from "../../src/blog-posts";
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
 
 const posts = getPosts();
 
 export default async (req, res) => {
+  await cors(req, res)
   res.json({ posts });
 };
